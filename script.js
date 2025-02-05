f175d9756469ec8adbbb802262bb03a747c1a135
aasheshwar24 <148005882+aasheshwar24@users.noreply.github.com>
2025-02-05 15:22:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
 // Mobile Navigation
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
+const navItems = document.querySelectorAll('.nav-links a'); // Get all navigation items
 
 hamburger.addEventListener('click', () => {
     navLinks.classList.toggle('active');
+});
+
+// Close menu when a link is clicked
+navItems.forEach(item => {
+    item.addEventListener('click', () => {
+        navLinks.classList.remove('active'); // Hide the menu
+    });
 });
 
 // Smooth Scrolling
